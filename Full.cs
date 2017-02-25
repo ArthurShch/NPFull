6f81c8fa57eade002e4b65a3427082428b14ef05
arch <arthurshcherbakov@gmail.com>
2017-02-25 23:59:55 +0700

    norm commit

---
+++ Full.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPFull
+{
+    class Full
+    {
+        public bool exitEnable = false;//выход в случае если итерации закончились
+        public int counter = 0;//счетчик количество итераций для рекурсивного алгоритма
+        public int countIter; //где хранится заданное количество итераций
+        public double bestRes = 0; //лучший результат 
+        public int[] besArray; // лучший массив на основании функции FunctionCheckResult
+
+        //просто пример функции оценки результата
+        // сумма первого и последнего элемента 
+        public int FunctionCheckResult(int[] arr)
+        {
+            return arr.First() + arr.Last();
+        }
+
+        //функция для уделения ранее размещённых элементов
+        public int[] Remove(int[] array, int index)
+        {
+            int[] arrayDelEl = new int[array.Length - 1];
+
+            //с начало
+            Array.Copy(array, 0, arrayDelEl, 0, index);
+
+            //после
+            //зделать проверки на последний
+            Array.Copy(array, index + 1, arrayDelEl, index, array.Length - index - 1);
+
+            return arrayDelEl;
+        }
+
+        //arrel - свободные элементы, arrplace - своб ячейки, result - текущая схема 
+        public void Recursion(int[] arrEl, int[] arrPlace, int[] result) 
+        {
+            int placeEl = -1;
+            for (int i = 0; i < arrEl.Length; i++)
+            {
+                placeEl = arrEl[i];
+                //новый массив элементов без одного, который был размещён
+                int[] nArrEl = Remove(arrEl, i);
+                int place = -1;
+                for (int j = 0; j < arrPlace.Length; j++)
+                {
+                    place = arrPlace[j];
+                    int[] nArrPlace = Remove(arrPlace, j);
+
+                    //полное копирование результа
+                    int[] nResult = new int[result.Length];
+                    Array.Copy(result, nResult, result.Length);                                        
+                    nResult[place] = placeEl;
+
+                    if (nArrEl.Length == 0)
+                    {
+                        //сдесь оценка резульата и его сохранение                         
+                        int nBestRes = FunctionCheckResult(nResult);
+                        if (bestRes < nBestRes)
+                        {
+                            bestRes = nBestRes;
+                            besArray = new int[nResult.Length];
+                            Array.Copy(nResult, besArray, nResult.Length);
+                        }
+
+                        //счётчик
+                        if (counter == countIter)
+                        {
+                            exitEnable = true;
+                            return;
+                        }
+                        counter++;
+                    }
+                    else
+                    {
+                        Recursion(nArrEl, nArrPlace, nResult);
+                        if (exitEnable)
+                        {
+                            return;
+                        }
+                    }
+
+                }// фор мест
+            }//фор елементов 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPFull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Full obj = new Full();
+
+            // ограничение на 1000 итераций
+            obj.countIter = 1000;
+
+            //создадим массив элементов
+            int[] arrEl = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrEl[i] = i;
+            }
+
+            //создадим массив позиций 
+            // у которого специально мест больше чем элементов
+            int[] arrPlace = new int[12];
+            int[] result = new int[12];
+
+            for (int i = 0; i < 12; i++)
+            {
+                arrPlace[i] = i;
+                result[i] = -1;
+            }
+
+            obj.Recursion(arrEl, arrPlace, result);
+
+            for (int i = 0; i < obj.besArray.Length; i++)
+            {
+                Console.Write(obj.besArray[i]);
+                Console.Write(' ');
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
